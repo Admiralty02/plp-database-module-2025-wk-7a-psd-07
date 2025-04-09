ca56a71ed221ead73734f0585662b5e14efe1e83
David Ochieng <160343230+Admiralty02@users.noreply.github.com>
2025-04-09 21:15:12 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -5,7 +5,7 @@ WITH SplitProducts AS (
         CustomerName,
         TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n), ',', -1)) AS Product
     FROM
-        Orders,
+        ProductDetails,
         (SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4) AS numbers
     WHERE
         SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n), ',', -1) <> ''
