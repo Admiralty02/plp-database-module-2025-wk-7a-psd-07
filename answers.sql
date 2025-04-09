b9c6b4562487ee7faa25499b7a969fdccf9aea6f
David Ochieng <160343230+Admiralty02@users.noreply.github.com>
2025-04-09 21:11:07 +0300

    Update answers.sql

---
+++ answers.sql
@@ -0,0 +1,18 @@
+--QUESTION ONE
+WITH SplitProducts AS (
+    SELECT
+        OrderID,
+        CustomerName,
+        TRIM(SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n), ',', -1)) AS Product
+    FROM
+        Orders,
+        (SELECT 1 AS n UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4) AS numbers
+    WHERE
+        SUBSTRING_INDEX(SUBSTRING_INDEX(Products, ',', n), ',', -1) <> ''
+)
+SELECT
+    OrderID,
+    CustomerName,
+    Product
+FROM
+    SplitProducts;
